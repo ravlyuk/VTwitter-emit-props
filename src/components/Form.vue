84bcefcc2bfda11c2e395c860a0c85530a25fcc1
evgeny.ravlyuk <yevhenii.ravliuk@jetsoftpro.com>
2021-12-29 12:24:39 +0200

    firdst init

---
+++ src/components/Form.vue
@@ -0,0 +1,31 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <textarea v-model="body" required placeholder="Type tweet here"/>
+    <button class="btn btnTweet" type="submit">Post Tweet</button>
+  </form>
+</template>
+
+<script>
+import {ref} from 'vue'
+
+export default {
+  emits: ['onSubmit'],
+
+  setup(_, {emit}) {
+    const body = ref('')
+
+    const onSubmit = () => {
+      emit('onSubmit', {
+        id: Math.round(Math.random() * 30),
+        avatar: `https://avatars.dicebear.com/api/male/${Date.now()}.svg`,
+        body: body.value,
+        likes: 0,
+        date: new Date(Date.now()).toLocaleString()
+      })
+      body.value = ''
+    }
+
+    return {body, onSubmit}
+  }
+}
+</script>
---
+++ src/components/List.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul class="tweets__wrapper">
+    <Item
+        v-for="item in items"
+        :key="item.id"
+        :item="item"
+    />
+  </ul>
+</template>
+
+<script>
+
+import Item from "@/components/Item.vue"
+
+
+export default {
+  components: {Item},
+  name: "List",
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
